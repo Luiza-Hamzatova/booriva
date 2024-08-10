2fcdd48986ab2ae7e8205cddc43698b094b0ae5e
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-08-10 21:55:13 +0300

    add: architecture and button.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import HomePage from "./pages/homePage/homePage";
-
+import Button from "./components/button/button";
 
 const App = () => {
   return (
-    <div className="App">
-     <HomePage/>
-     hello
+    <div className="App wrapper">
+      <Button value="смотреть все" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/button/button.jsx
@@ -0,0 +1,14 @@
+import ButtonSvg from './../../assets/svg/buttonSvg'
+
+import styles from './button.module.sass'
+
+
+const Button = ({value}) => {
+    return <div className={styles.button}>
+        <div className={styles.bg}><ButtonSvg/></div>
+        <div className={styles.text}>{value}</div>
+    </div>
+    
+}
+
+export default Button
