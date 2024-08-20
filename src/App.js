5122c5a24ac6fd381b2c69e8f6e4afe2cdf6107c
Zakrieva <zakrieva34@mail.ru>
2024-08-20 19:27:28 +0300

    add: basket

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Button from "./components/button/button";
 import CardProduct from "./components/cardProduct/cardProduct";
-
+import Basket from "./layout/basket/basket";
 
 const App = () => {
   return (
     <div className="App wrapper">
-
-      
-      <CardProduct  img={"./img/productImg.png"} id={0} price={'1 099'} name={'Cвитшот вставка клетка'}/>
+      {/* <Button value={'смотреть все'}/> */}
+      {/* <CardProduct  img={"./img/productImg.png"} id={0} price={'1 099'} name={'Cвитшот вставка клетка'}/> */}
+      <Basket/>
     </div>
   );
 };
