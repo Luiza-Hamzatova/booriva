d7a49e23fd56e511de9a6bb10034f203ef7caa40
Luiza-Hamzatova <hamzatovaluiza1998@mail.ru>
2024-08-24 18:18:23 +0300

    ю

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Footer from "./layout/Footer/Footer";
 import Header from "./layout/Header/Header";
 import InfiniteMarquee from "./layout/InfiniteMarquee/InfiniteMarquee";
@@ -6,26 +5,22 @@ import Filter from "./pages/Filter/Filter";
 import Contact from "./layout/Contact/contact";
 import NewColection from "./layout/NewColection/newColection";
 import NewCatalog from "./layout/NewCatalog/newCatalog";
-import Baner from './layout/Baner'
+import Baner from "./layout/Baner";
 import BoorivaStore from "./components/boorivaStore/boorivaStore";
 const App = () => {
   return (
     <div>
-     
-       <Header /> 
-       <Filter/>
-      <NewColection/> 
-      <NewCatalog/>
-      <Baner/>
-      <BoorivaStore/>
-      <InfiniteMarquee/>
-      <Contact/>
+      <Header />
+      <Filter />
+      <NewColection />
+      <NewCatalog />
+      <Baner />
+      <BoorivaStore />
+      <InfiniteMarquee />
+      <Contact />
       <Footer />
     </div>
   );
 };
 
 export default App;
-
-
-
--- src/components/boorivaStore/boorivaStore.jsx
+++ src/components/boorivaStore/boorivaStore.jsx
@@ -8,63 +8,61 @@ import greaterThanSignS from "../../assets/svg/greaterThanSignS.svg";
 import greaterThanSignL from "../../assets/svg/greaterThanSignL.svg";
 import heartS from "../../assets/svg/heartS.svg";
 
-
 import styles from "./boorivaStore.module.sass";
 
 const BoorivaStore = () => {
-    return (
-      <div className={styles.boorivaStore}>
-        <h1 className={styles.title}>
-          <span>BOORIVA</span> <span>STORE</span>
-        </h1>
-        <div className={styles.info}>
-          <p>
-            г.Киев, <br /> ул. Нижний Вал, 37
-          </p>
-          <br />
-          <p>
-            Ежедневно <br />с 10:00 до 21:00
-          </p>
-        </div>
-        <div className={styles.boorivaStoreImg}>
-          <img
-            src={verticalTilde}
-            alt="A vertical tilde icon"
-            className={styles.verticalTilde}
-          />
-          <img
-            src={horizontalTilde}
-            alt="A horizontal tilde icon"
-            className={styles.horizontalTilde}
-          />
-          <img src={prime} alt="A prime icon" className={styles.prime} />
-          <img
-            src={squigglyLine}
-            alt="A squiggly line icon"
-            className={styles.squigglyLine}
-          />
-          <img src={heartL} alt="A heart icon" className={styles.heartL} />
-          <img src={tie} alt="A tie icon" className={styles.tie2} />
-          <img src={tie} alt="A tie icon" className={styles.tie} />
-          <img
-            src={greaterThanSignS}
-            alt="A greater than sign icon"
-            className={styles.greaterThanSignS}
-          />
-          <img
-            src={greaterThanSignL}
-            alt="A greater than sign icon"
-            className={styles.greaterThanSignL}
-          />
-          <img src={heartS} alt="A heart icon" className={styles.heartS} />
-        </div>
-        <p className={styles.description}>
-          Ты можешь заказать примерку понравившейся вещи или просто заглянуть,
-          гуляя по Подолу, и примерять что-то из в наличии
+  return (
+    <div className={styles.boorivaStore}>
+      <h1 className={styles.title}>
+        <span>BOORIVA</span> <span>STORE</span>
+      </h1>
+      <div className={styles.info}>
+        <p>
+          г.Киев, <br /> ул. Нижний Вал, 37
+        </p>
+        <br />
+        <p>
+          Ежедневно <br />с 10:00 до 21:00
         </p>
       </div>
-    );
-  };
-  
-  export default BoorivaStore;
-  
+      <div className={styles.boorivaStoreImg}>
+        <img
+          src={verticalTilde}
+          alt="A vertical tilde icon"
+          className={styles.verticalTilde}
+        />
+        <img
+          src={horizontalTilde}
+          alt="A horizontal tilde icon"
+          className={styles.horizontalTilde}
+        />
+        <img src={prime} alt="A prime icon" className={styles.prime} />
+        <img
+          src={squigglyLine}
+          alt="A squiggly line icon"
+          className={styles.squigglyLine}
+        />
+        <img src={heartL} alt="A heart icon" className={styles.heartL} />
+        <img src={tie} alt="A tie icon" className={styles.tie2} />
+        <img src={tie} alt="A tie icon" className={styles.tie} />
+        <img
+          src={greaterThanSignS}
+          alt="A greater than sign icon"
+          className={styles.greaterThanSignS}
+        />
+        <img
+          src={greaterThanSignL}
+          alt="A greater than sign icon"
+          className={styles.greaterThanSignL}
+        />
+        <img src={heartS} alt="A heart icon" className={styles.heartS} />
+      </div>
+      <p className={styles.description}>
+        Ты можешь заказать примерку понравившейся вещи или просто заглянуть,
+        гуляя по Подолу, и примерять что-то из в наличии
+      </p>
+    </div>
+  );
+};
+
+export default BoorivaStore;
