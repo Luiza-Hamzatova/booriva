fe7c4984b61284eea03e20c13ede72b676dfc9df
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-08-28 12:32:27 +0300

    .

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
       </Routes>
-
       <Footer />
     </div>
   );
