cdb77dd52bafebabfa5cb5e011e3034b230a590c
Luiza-Hamzatova <hamzatovaluiza1998@mail.ru>
2024-08-31 20:12:57 +0300

    /

--- src/pages/Wishlist/index.jsx
+++ src/pages/Wishlist/index.jsx
@@ -1,12 +1,12 @@
 import EmptyWishlist from "./EmptyWishlist/EmptyWishlist";
 import CardProduct from "../../components/cardProduct/cardProduct";
 import styles from "./index.module.sass";
-import Contact from "../../layout/Contact/contact";
+import Contact from "../../components/Contact/contact";
 import Delivery from "../../components/delivery/delivery";
 import { useState } from "react";
 
 const Wishlist = () => {
-  const [products, setProducts] = useState([1, 2, 3]);
+  const [products, setProducts] = useState([7]);
   if (products && products.length > 0) {
   }
 
@@ -17,8 +17,15 @@ const Wishlist = () => {
           <h1 className={styles.title}>Список желаний</h1>
           <p className={styles.subtitle}>твой тайный Список желаний</p>
         </div>
-        <div className={styles.cards}>
-          {products.length > 0 ? <CardProduct /> : <EmptyWishlist />}
+        <div>
+          {products.length > 0 ? (
+            <div className={styles.cards}>
+              {" "}
+              <CardProduct />
+            </div>
+          ) : (
+            <EmptyWishlist />
+          )}
         </div>
       </div>
       <Delivery />
