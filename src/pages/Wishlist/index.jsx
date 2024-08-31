55a8d8c98f7cb78bf1d821e03fe0ddce1c0947b5
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-09-01 01:13:35 +0300

    fix: conflicts

--- src/pages/Wishlist/index.jsx
+++ src/pages/Wishlist/index.jsx
@@ -1,8 +1,7 @@
 import EmptyWishlist from "./EmptyWishlist/EmptyWishlist";
 import CardProduct from "../../components/cardProduct/cardProduct";
 import styles from "./index.module.sass";
-import Contact from "../../components/Contact/contact";
-import Contact from "../../components/Contact/contact";
+import Contact from "./../../components/Contact/contact";
 import Delivery from "../../components/delivery/delivery";
 import { useState } from "react";
 
