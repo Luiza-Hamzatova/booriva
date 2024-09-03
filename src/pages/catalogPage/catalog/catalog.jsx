2c2c8a4cf0ddc64e47b9fd81d58ff9b5502c6bd6
Luiza-Hamzatova <hamzatovaluiza1998@mail.ru>
2024-09-03 17:17:46 +0300

    категории показывает

--- src/pages/catalogPage/catalog/catalog.jsx
+++ src/pages/catalogPage/catalog/catalog.jsx
@@ -1,34 +1,44 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import qs from "qs";
+
 import CardProduct from "../../../components/cardProduct/cardProduct";
 import Title from "../../../components/title/title";
-import styles from "./catalog.module.sass";
 import Filter from "../filter/filter";
-import { useLocation } from "react-router-dom";
 import Baner from "../../homePage/Baner/baner";
 
-const Catalog = () => {
+import styles from "./catalog.module.sass";
+
+const Catalog = ({ activeSubmenuItem, setActiveSubmenuItem }) => {
   const [products, setProducts] = useState([]);
+  const [menuId, setMenuId] = useState("");
+  const [categoryId, setCategoryId] = useState("");
   const location = useLocation();
-  useEffect(() => {
-    const menuId = qs.parse(location.search.substring(1)).menuId;
-    fetch(
-      `https://65588446e93ca47020a966c9.mockapi.io/menuCatalog?menuId=${menuId}`
-    )
-      .then((res) => res.json())
-      .then((res) => setProducts(res[0].products));
-  }, [location]);
 
   useEffect(() => {
-    const categoryId = qs.parse(location.search.substring(1)).categoryId;
-    fetch(
-      `https://65588446e93ca47020a966c9.mockapi.io/categoriesCatalog?categoryId=${categoryId}`
-    )
-      .then((res) => res.json())
-      .then((res) => setProducts(res[0].products));
+    const queryParams = qs.parse(location.search.substring(1));
+    const { menuId, categoryId } = queryParams;
+
+    if (menuId) {
+      setMenuId(menuId);
+      fetch(
+        `https://65588446e93ca47020a966c9.mockapi.io/menuCatalog?menuId=${menuId}`
+      )
+        .then((res) => res.json())
+        .then((res) => setProducts(res[0].products));
+    }
+
+    if (categoryId) {
+      setCategoryId(categoryId);
+      fetch(
+        `https://65588446e93ca47020a966c9.mockapi.io/categoriesCatalog?categoryId=${categoryId}`
+      )
+        .then((res) => res.json())
+        .then((res) => setProducts(res[0].products));
+    }
   }, [location]);
 
-  console.log(products);
+  console.log(products.menuName);
 
   return (
     <div className="wrapper">
@@ -38,7 +48,10 @@ const Catalog = () => {
           <Title valueh1={menuId} valueh2={categoryId} />
         </div>
         <div className={styles.catalogDown}>
-          <Filter />
+          <Filter
+            activeSubmenuItem={activeSubmenuItem}
+            setActiveSubmenuItem={setActiveSubmenuItem}
+          />
           <div className={styles.catalog_products}>
             {products.map(({ id, images, name, price }) => (
               <CardProduct
--- src/pages/catalogPage/filter/filter.jsx
+++ src/pages/catalogPage/filter/filter.jsx
@@ -1,62 +1,116 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import qs from "qs";
+
 import styles from "./filter.module.sass";
 
-const Filter = () => {
+const Filter = ({ activeSubmenuItem, setActiveSubmenuItem }) => {
+  const [selectedPrice, setSelectedPrice] = useState("desktopAllPrices");
+  const [categories, setCategories] = useState([]);
+  const params = qs.parse(window.location.search.substring(1));
+
+  useEffect(() => {
+    fetch(
+      `https://640ef1d54ed25579dc40e2a6.mockapi.io/categories/a${params.menuId}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data.categories);
+      });
+  }, [params.menuId]);
+
+  const handlePriceChange = (e) => {
+    e.persist();
+    setSelectedPrice(e.target.value);
+  };
+
   return (
-    
-      <div className={styles.filter}>
+    <div className={styles.filter}>
+      {categories && categories.length > 0 && (
         <div className={styles.column}>
           <div className={styles.title}>КАТЕГОРИИ:</div>
           <ul className={styles.list}>
-            <li>
-              <a href="/" className={styles.item}>
-                Пиджаки
-              </a>
-            </li>
-            <li>
-              <a href="/" className={styles.item}>
-                Рубашки
-              </a>
-            </li>
-            <li>
-              <a href="/" className={styles.item}>
-                Свитшоты
-              </a>
-            </li>
-            <li>
-              <a href="/" className={styles.item}>
-                Футболки
-              </a>
-            </li>
+            {categories.map(({ id, name }) => (
+              <li key={id}>
+                <Link
+                  to={`?menuId=${params.menuId}&categoryId=${id}`}
+                  className={
+                    styles.item +
+                    " " +
+                    (id === activeSubmenuItem ? styles.active : "")
+                  }
+                  onClick={() => {
+                    setActiveSubmenuItem(id);
+                  }}
+                >
+                  {name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
-
-        <div className={styles.column}>
-          <div className={styles.title}>ЦЕНЫ:</div>
-          <form className={styles.options}>
-            <div className={styles.option}>
-              <input name="price" type="radio" id="AllPrices" />
-              <label htmlFor="AllPrices">Все цены </label>
-            </div>
-            <div className={styles.option}>
-              <input name="price" type="radio" id="to500" />
-              <label htmlFor="to500">до 500</label>
-            </div>
-            <div className={styles.option}>
-              <input name="price" type="radio" id="from500-to1000" />
-              <label htmlFor="from500-to1000">500 — 1000</label>
-            </div>
-            <div className={styles.option}>
-              <input name="price" type="radio" id="from1000-to1500" />
-              <label htmlFor="from1000-to1500">1000 — 1500</label>
-            </div>
-            <div className={styles.option}>
-              <input name="price" type="radio" id="from1500" />
-              <label htmlFor="from1500">от 1500</label>
-            </div>
-          </form>
-        </div>
+      )}
+      <div className={styles.column}>
+        <div className={styles.title}>ЦЕНЫ:</div>
+        <form action="" className={styles.options}>
+          <div className={styles.option}>
+            <input
+              type="radio"
+              id="desktopAllPrices"
+              name="price"
+              value="desktopAllPrices"
+              checked={selectedPrice === "desktopAllPrices"}
+              onChange={handlePriceChange}
+            />
+            <label htmlFor="desktopAllPrices">Все цены</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="radio"
+              id="desktopUpto500"
+              name="price"
+              value="desktopUpto500"
+              checked={selectedPrice === "desktopUpto500"}
+              onChange={handlePriceChange}
+            />
+            <label htmlFor="desktopUpto500">до 500</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="radio"
+              id="desktopFrom500to1000"
+              name="price"
+              value="desktopFrom500to1000"
+              checked={selectedPrice === "desktopFrom500to1000"}
+              onChange={handlePriceChange}
+            />
+            <label htmlFor="desktopFrom500to1000">500 — 1000</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="radio"
+              id="desktopFrom1000to1500"
+              name="price"
+              value="desktopFrom1000to1500"
+              checked={selectedPrice === "desktopFrom1000to1500"}
+              onChange={handlePriceChange}
+            />
+            <label htmlFor="desktopFrom1000to1500">1000 — 1500</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="radio"
+              id="desktopFrom1500"
+              name="price"
+              value="desktopFrom1500"
+              checked={selectedPrice === "desktopFrom1500"}
+              onChange={handlePriceChange}
+            />
+            <label htmlFor="desktopFrom1500">от 1500</label>
+          </div>
+        </form>
       </div>
-  
+    </div>
   );
 };
 
