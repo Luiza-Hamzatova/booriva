846cde0ca8f883afcb25ff7504e7e01b1b1df99b
Zakrieva <zakrieva34@mail.ru>
2024-09-05 19:40:20 +0300

    fix: title

--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -3,8 +3,8 @@ import styles from "./title.module.sass";
 const Title = ({ valueh1, valueh2 }) => {
   return (
     <div className={styles.title}>
-      <h1 className={styles.titleh1}>{valueh1}</h1>
-      <div className={styles.titleh2}>
+      <h1 className={styles.title_h1}>{valueh1}</h1>
+      <div className={styles.title_h2}>
         <h2>{valueh2}</h2>
         <img src="./img/Vector.svg" alt="" />
       </div>
